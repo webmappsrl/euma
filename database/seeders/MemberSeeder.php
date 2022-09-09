d3e9d2a80f1e4a1acdcfc8da27c1ca4279867617
Pedram Katanchi <pedramkat@gmail.com>
2022-09-09 16:33:08 +0200

    Added member seeder

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Member;
 use Illuminate\Database\Seeder;
 
 class MemberSeeder extends Seeder
@@ -13,6 +14,27 @@ class MemberSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Member::truncate();
+  
+        $csvFile = fopen(base_path("storage/app/seeder/member.csv"), "r");
+  
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ",")) !== FALSE) {
+            if (!$firstline) {
+                Member::create([
+                    'name_en' => $data['1'],
+                    'name_orig' => $data['2'],
+                    'acronym' => $data['3'],
+                    'country' => $data['4'],
+                    'web' => $data['5'],
+                    'members' => $data['6'],
+                    'since' => $data['7'],
+                    'type' => $data['8']
+                ]);    
+            }
+            $firstline = false;
+        }
+   
+        fclose($csvFile);
     }
 }
