9481df250dd77a85fd0171a03107e21a2130c3de
Pedram Katanchi <pedramkat@gmail.com>
2022-09-13 15:29:28 +0200

    Revert "Creating nova component map multi linestring"
    This reverts commit 48d932f9d002693ee45ac074b8d59402056f37ac.

--- nova-components/MapMultiLinstring/resources/js/field.js
+++
@@ -1,9 +0,0 @@
-import IndexField from './components/IndexField'
-import DetailField from './components/DetailField'
-import FormField from './components/FormField'
-
-Nova.booting((app, store) => {
-  app.component('index-map-multi-linstring', IndexField)
-  app.component('detail-map-multi-linstring', DetailField)
-  app.component('form-map-multi-linstring', FormField)
-})
