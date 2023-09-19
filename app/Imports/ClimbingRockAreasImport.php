e91798bb234c9267293cd7e0d1bec04fe4d4952b
Pedram Katanchi <pedramkat@gmail.com>
2023-09-19 14:28:42 +0200

    added miscelaneus changes

--- app/Imports/ClimbingRockAreasImport.php
+++ app/Imports/ClimbingRockAreasImport.php
@@ -17,12 +17,11 @@ class ClimbingRockAreasImport implements ToCollection, WithHeadingRow
     public function collection($rows)
     {
         $failed_ids = [];
-        foreach ($rows as $row) 
-        {
+        foreach ($rows as $row) {
             try {
                 $lat = $row['lat'];
                 $lng = $row['lng'];
-                $member = Member::where('acronym',$row['member_acronym'])->get()[0];
+                $member = Member::where('acronym', $row['member_acronym'])->get()[0];
 
                 $ClimbingRockArea = ClimbingRockArea::updateOrCreate(
                     [
@@ -30,26 +29,29 @@ class ClimbingRockAreasImport implements ToCollection, WithHeadingRow
                         'member_id' => $member->id
                     ],
                     [
-                        'description' => ($row['description'])?$row['description']:'',
+                        'english_description' => ($row['english_description']) ? $row['english_description'] : '',
+                        'original_description' => ($row['original_description']) ? $row['original_description'] : '',
                         'url' => $row['url'],
-                        'local_rules_url' => ($row['local_rules_url'])?$row['local_rules_url']:'',
-                        'local_rules_description' => ($row['local_rules_description'])?$row['local_rules_description']:'',
-                        'location_quality' => ($row['location_quality'])?$row['location_quality']:null,
-                        'routes_number' => ($row['routes_number'])?$row['routes_number']:null,
-                        'geometry'    => DB::select("SELECT ST_GeomFromText('POINT($lng $lat)') As wkt")[0]->wkt, 
-                        'elevation'    => ($row['elevation'])?$row['elevation']:null,
-                        'local_restrictions'    => ($row['local_restrictions'] == 'yes')?true:false,
-                        'local_restrictions_description' => ($row['local_restrictions_description'])?$row['local_restrictions_description']:'',
+                        'local_rules_url' => ($row['local_rules_url']) ? $row['local_rules_url'] : '',
+                        'english_local_rules_description' => ($row['english_local_rules_description']) ? $row['english_local_rules_description'] : '',
+                        'original_local_rules_description' => ($row['original_local_rules_description']) ? $row['original_local_rules_description'] : '',
+                        'location_quality' => ($row['location_quality']) ? $row['location_quality'] : null,
+                        'routes_number' => ($row['routes_number']) ? $row['routes_number'] : null,
+                        'geometry'    => DB::select("SELECT ST_GeomFromText('POINT($lng $lat)') As wkt")[0]->wkt,
+                        'elevation'    => ($row['elevation']) ? $row['elevation'] : null,
+                        'local_restrictions'    => ($row['local_restrictions'] == 'yes') ? true : false,
+                        'english_local_restrictions_description' => ($row['english_local_restrictions_description']) ? $row['english_local_restrictions_description'] : '',
+                        'original_local_restrictions_description' => ($row['original_local_restrictions_description']) ? $row['original_local_restrictions_description'] : '',
                     ]
                 );
-                
-                if (isset($row['original_name']) && !empty($row['original_name'])){
+
+                if (isset($row['original_name']) && !empty($row['original_name'])) {
                     $ClimbingRockArea->original_name = $row['original_name'];
                 }
-                if (isset($row['english_name']) && !empty($row['english_name'])){
+                if (isset($row['english_name']) && !empty($row['english_name'])) {
                     $ClimbingRockArea->original_name = $row['english_name'];
                 }
-                if (isset($row['name']) && !empty($row['name'])){
+                if (isset($row['name']) && !empty($row['name'])) {
                     $ClimbingRockArea->original_name = $row['name'];
                 }
 
@@ -58,22 +60,22 @@ class ClimbingRockAreasImport implements ToCollection, WithHeadingRow
                     $parking_lng = $row['parking_lng'];
                     $ClimbingRockArea->parking_position = DB::select("SELECT ST_GeomFromText('POINT($parking_lng $parking_lat)') As wkt")[0]->wkt;
                 }
-                
+
                 if ($row['climbing_style']) {
-                    $styles = explode(',',$row['climbing_style']);
+                    $styles = explode(',', $row['climbing_style']);
                     foreach ($styles as $style) {
-                        $ClimbingStyle = ClimbingStyle::where('identifier',strtolower($style))->get();
+                        $ClimbingStyle = ClimbingStyle::where('identifier', strtolower($style))->get();
                         if (!empty($ClimbingStyle) && count($ClimbingStyle) > 0) {
                             $ClimbingRockArea->climbingStyles()->attach($ClimbingStyle[0]->id);
                         }
                     }
                 }
-                
+
                 if ($row['climbing_rock_type']) {
-                    $types = explode(',',$row['climbing_rock_type']);
+                    $types = explode(',', $row['climbing_rock_type']);
                     foreach ($types as $type) {
                         $type = str_replace(' ', '-', $type);
-                        $ClimbingRockType = ClimbingRockType::where('identifier',strtolower($type))->get();
+                        $ClimbingRockType = ClimbingRockType::where('identifier', strtolower($type))->get();
                         if (!empty($ClimbingRockType) && count($ClimbingRockType) > 0) {
                             $ClimbingRockArea->ClimbingRockTypes()->attach($ClimbingRockType[0]->id);
                         }
@@ -82,7 +84,7 @@ class ClimbingRockAreasImport implements ToCollection, WithHeadingRow
 
                 $ClimbingRockArea->save();
             } catch (Exception $e) {
-                array_push($failed_ids,$row['member_acronym'].' - '.$row['id']);
+                array_push($failed_ids, $row['member_acronym'].' - '.$row['id']);
                 Log::info('Error creating Crags from '. $row['member_acronym'] .' with id: '.$row['id']."\n ERROR: ".$e->getMessage());
             }
             if ($failed_ids) {
@@ -91,5 +93,5 @@ class ClimbingRockAreasImport implements ToCollection, WithHeadingRow
                 }
             }
         }
-    } 
+    }
 }
