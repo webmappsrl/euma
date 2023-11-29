8a5706fbfc694d666c0edbddf72fe0d8493343af
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-11-29 16:17:23 +0100

    Update casts in Member and TrailSurvey models

--- app/Models/TrailSurvey.php
+++ app/Models/TrailSurvey.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\TrailUserTypes;
+use App\Enums\MaintenanceOperatorTypes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\AsEnumCollection;
 
 class TrailSurvey extends Model
 {
+    protected $casts = [
+        'other_trails_users' => AsEnumCollection::class . ':' . TrailUserTypes::class,
+        'trails_maintenance_done_by' => AsEnumCollection::class . ':' . MaintenanceOperatorTypes::class,
+        'contact_completion_date' => 'datetime:Y-m-d'
+    ];
     use HasFactory;
 
     public function member()
