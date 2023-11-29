e350c4aaef837b16a78138d14f357f25f0fa1561
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-11-29 12:59:37 +0100

    Add tabs and fields for Hut Survey resource

--- app/Nova/HutSurvey.php
+++ app/Nova/HutSurvey.php
@@ -4,8 +4,11 @@ namespace App\Nova;
 
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Eminiarts\Tabs\Tabs;
+use Eminiarts\Tabs\Tab;
 
 class HutSurvey extends Resource
 {
@@ -41,8 +44,18 @@ class HutSurvey extends Resource
     public function fields(NovaRequest $request)
     {
         return [
-            ID::make()->sortable(),
-            BelongsTo::make('Member')->searchable(),
+            new Tabs('Hut Survey', [
+                new Tab('General Info', $this->generalInfoFields()),
+                new Tab('Waste Water Treatment', $this->wasteWaterTreatmentFields()),
+                new Tab('Waste Management', $this->wasteManagementFields()),
+                new Tab('Water Supply', $this->waterSupplyFields()),
+                new Tab('Source of Energy', $this->sourceOfEnergyFields()),
+                new Tab('National Park', $this->nationalParkFields()),
+                new Tab('Sanitary', $this->sanitaryFields()),
+                new Tab('Kitchen', $this->kitchenFields()),
+                new Tab('Hut Type', $this->hutTypeFields()),
+                new Tab('Ownership', $this->ownershipFields()),
+            ]),
         ];
     }
 
@@ -88,5 +101,150 @@ class HutSurvey extends Resource
     public function actions(NovaRequest $request)
     {
         return [];
+    }
+
+
+    //general info fields
+    public function generalInfoFields()
+    {
+        return [
+            ID::make()->sortable(),
+            BelongsTo::make('Member')->searchable(),
+        ];
+    }
+
+    //waste water treatment fields
+    public function wasteWaterTreatmentFields()
+    {
+        return [
+            Number::make('Mechanical + Biological Wastewater Treatment', 'wastewater_mechanical_plus_biological_treatment')->nullable(),
+            Number::make('Mechanical + Biological Wastewater Treatment Percentage', 'wastewater_mechanical_plus_biological_treatment_percentage')->nullable(),
+            Number::make('No Wastewater Treatment', 'wastewater_no_treatment')->nullable(),
+            Number::make('No Wastewater Treatment Percentage', 'wastewater_no_treatment_percentage')->nullable(),
+            Number::make('Public Grid Wastewater', 'wastewater_public_grid')->nullable(),
+            Number::make('Public Grid Wastewater Percentage', 'wastewater_public_grid_percentage')->nullable(),
+            Number::make('Unspecified Wastewater', 'wastewater_unspecified')->nullable(),
+            Number::make('Unspecified Wastewater Percentage', 'wastewater_unspecified_percentage')->nullable(),
+            Number::make('Wastewater Treatment Grand Total', 'wastewater_treatment_grand_total')->nullable(),
+        ];
+    }
+
+    //waste management fields
+    public function wasteManagementFields()
+    {
+        return [
+            Number::make('Waste Management System Yes', 'waste_management_system_yes')->nullable(),
+            Number::make('Waste Management System Yes Percentage', 'waste_management_system_yes_percentage')->nullable(),
+            Number::make('Waste Management System No', 'waste_management_system_no')->nullable(),
+            Number::make('Waste Management System No Percentage', 'waste_management_system_no_percentage')->nullable(),
+            Number::make('Waste Management System Grand Total', 'waste_management_system_grand_total')->nullable(),
+        ];
+    }
+
+    //water supply fields
+    public function waterSupplyFields()
+    {
+        return [
+            Number::make('Surface Water Supply', 'surface_water_supply')->nullable(),
+            Number::make('Surface Water Supply Percentage', 'surface_water_supply_percentage')->nullable(),
+            Number::make('Ground Well Water Supply', 'ground_well_water_supply')->nullable(),
+            Number::make('Ground Well Water Supply Percentage', 'ground_well_water_supply_percentage')->nullable(),
+            Number::make('Public Supply', 'public_supply')->nullable(),
+            Number::make('Public Supply Percentage', 'public_supply_percentage')->nullable(),
+            Number::make('No Water Supply', 'no_water_supply')->nullable(),
+            Number::make('No Water Supply Percentage', 'no_water_supply_percentage')->nullable(),
+            Number::make('Water Supply Grand Total', 'water_supply_grand_total')->nullable(),
+        ];
+    }
+
+    //source of energy fields
+    public function sourceOfEnergyFields()
+    {
+        return [
+            Number::make('Public Grid Energy Source', 'source_of_energy_public_grid')->nullable(),
+            Number::make('Public Grid Energy Source Percentage', 'source_of_energy_public_grid_percentage')->nullable(),
+            Number::make('Renewable Energy Source', 'source_of_energy_renewable')->nullable(),
+            Number::make('Renewable Energy Source Percentage', 'source_of_energy_renewable_percentage')->nullable(),
+            Number::make('Generator Energy Source', 'source_of_energy_generator')->nullable(),
+            Number::make('Generator Energy Source Percentage', 'source_of_energy_generator_percentage')->nullable(),
+            Number::make('No Energy Source', 'no_source_of_energy')->nullable(),
+            Number::make('No Energy Source Percentage', 'no_source_of_energy_percentage')->nullable(),
+            Number::make('Source of Energy Grand Total', 'source_of_energy_grand_total')->nullable(),
+        ];
+    }
+
+    //national park fields
+    public function nationalParkFields()
+    {
+        return [
+            Number::make('National Parks', 'national_parks')->nullable(),
+            Number::make('National Parks Percentage', 'national_parks_percentage')->nullable(),
+            Number::make('Protected Areas', 'protected_areas')->nullable(),
+            Number::make('Protected Areas Percentage', 'protected_areas_percentage')->nullable(),
+            Number::make('No National Parks', 'no_national_parks')->nullable(),
+            Number::make('No National Parks Percentage', 'no_national_parks_percentage')->nullable(),
+            Number::make('National Parks Grand Total', 'national_parks_grand_total')->nullable(),
+        ];
+    }
+
+    //sanitary fields
+    public function sanitaryFields()
+    {
+        return [
+            Number::make('Dry Toilets Sanitary', 'sanitary_dry_toilets')->nullable(),
+            Number::make('Wet Toilets Sanitary', 'sanitary_wet_toilets')->nullable(),
+            Number::make('Separation Toilets Sanitary', 'sanitary_toilets_separation')->nullable(),
+            Number::make('Total Toilets Sanitary', 'sanitary_toilets')->nullable(),
+            Number::make('Showers Sanitary', 'sanitary_showers')->nullable(),
+            Number::make('Total With Toilets Sanitary', 'sanitary_total_with_toilets')->nullable(),
+            Number::make('Total With Toilets Percentage Sanitary', 'sanitary_total_with_toilets_percentage')->nullable(),
+            Number::make('Total Without Toilets Sanitary', 'sanitary_total_without_toilets')->nullable(),
+            Number::make('Total Without Toilets Percentage Sanitary', 'sanitary_total_without_toilets_percentage')->nullable(),
+        ];
+    }
+
+    //kitchen fields
+    public function kitchenFields()
+    {
+        return [
+            Number::make('Electric Kitchen', 'electric_kitchen')->nullable(),
+            Number::make('Electric Kitchen Percentage', 'electric_kitchen_percentage')->nullable(),
+            Number::make('Gas Kitchen', 'gas_kitchen')->nullable(),
+            Number::make('Gas Kitchen Percentage', 'gas_kitchen_percentage')->nullable(),
+            Number::make('Wood Kitchen', 'wood_kitchen')->nullable(),
+            Number::make('Wood Kitchen Percentage', 'wood_kitchen_percentage')->nullable(),
+            Number::make('No Kitchen', 'no_kitchen')->nullable(),
+            Number::make('No Kitchen Percentage', 'no_kitchen_percentage')->nullable(),
+            Number::make('Kitchen Grand Total', 'kitchen_grand_total')->nullable(),
+        ];
+    }
+
+    //hut type fields
+    public function hutTypeFields()
+    {
+        return [
+            Number::make('Hut Type Hut', 'hut_type_hut')->nullable(),
+            Number::make('Hut Type Biwak', 'hut_type_biwak')->nullable(),
+            Number::make('Hut Type Cabane', 'hut_type_cabane')->nullable(),
+            Number::make('Hut Type Other', 'hut_type_other')->nullable(),
+            Number::make('Hut Type Grand Total', 'hut_type_grand_total')->nullable(),
+            Number::make('Lowest Elevation Hut Type', 'hut_type_lowest_elevation')->nullable(),
+            Number::make('Highest Elevation Hut Type', 'hut_type_highest_elevation')->nullable(),
+        ];
+    }
+
+    //ownership fields
+    public function ownershipFields()
+    {
+        return [
+            Number::make('Mountaineering Association Ownership', 'ownership_mountaineering_association')->nullable(),
+            Number::make('Mountain Club Ownership', 'ownership_mountain_club')->nullable(),
+            Number::make('EOS POA SOX SEO XOS SHO XOO Ownership', 'ownership_eos_poa_sox_seo_xos_sho_xoo')->nullable(),
+            Number::make('Municipality Ownership', 'ownership_municipality')->nullable(),
+            Number::make('State Ownership', 'ownership_state')->nullable(),
+            Number::make('Private Property Ownership', 'ownership_private_property')->nullable(),
+            Number::make('Other Ownership', 'ownership_other')->nullable(),
+            Number::make('Ownership Grand Total', 'ownership_grand_total')->nullable(),
+        ];
     }
 }
--- database/migrations/2023_11_29_082732_add_fields_to_hut_surveys_table.php
+++ database/migrations/2023_11_29_082732_add_fields_to_hut_surveys_table.php
@@ -15,23 +15,94 @@ return new class extends Migration
     {
         Schema::table('hut_surveys', function (Blueprint $table) {
             //waste water treatment
-            $table->integer('mechanical_plus_biological_treatment')->nullable();
-            $table->integer('no_treatment')->nullable();
-            $table->integer('public_grid')->nullable();
-            $table->integer('unspecified')->nullable();
+            $table->integer('wastewater_mechanical_plus_biological_treatment')->nullable();
+            $table->decimal('wastewater_mechanical_plus_biological_treatment_percentage', 5, 2)->nullable();
+            $table->integer('wastewater_no_treatment')->nullable();
+            $table->decimal('wastewater_no_treatment_percentage', 5, 2)->nullable();
+            $table->integer('wastewater_public_grid')->nullable();
+            $table->decimal('wastewater_public_grid_percentage', 5, 2)->nullable();
+            $table->integer('wastewater_unspecified')->nullable();
+            $table->decimal('wastewater_unspecified_percentage', 5, 2)->nullable();
             $table->integer('wastewater_treatment_grand_total')->nullable();
 
             //waste management system
             $table->integer('waste_management_system_yes')->nullable();
+            $table->decimal('waste_management_system_yes_percentage', 5, 2)->nullable();
             $table->integer('waste_management_system_no')->nullable();
+            $table->decimal('waste_management_system_no_percentage', 5, 2)->nullable();
             $table->integer('waste_management_system_grand_total')->nullable();
 
             //water supply
-            $table->integer('surface_water')->nullable();
-            $table->integer('ground_well_water')->nullable();
+            $table->integer('surface_water_supply')->nullable();
+            $table->decimal('surface_water_supply_percentage', 5, 2)->nullable();
+            $table->integer('ground_well_water_supply')->nullable();
+            $table->decimal('ground_well_water_supply_percentage', 5, 2)->nullable();
             $table->integer('public_supply')->nullable();
-            $table->integer('none')->nullable();
+            $table->decimal('public_supply_percentage', 5, 2)->nullable();
+            $table->integer('no_water_supply')->nullable();
+            $table->decimal('no_water_supply_percentage', 5, 2)->nullable();
             $table->integer('water_supply_grand_total')->nullable();
+
+            //source of energy
+            $table->integer('source_of_energy_public_grid')->nullable();
+            $table->decimal('source_of_energy_public_grid_percentage', 5, 2)->nullable();
+            $table->integer('source_of_energy_renewable')->nullable();
+            $table->decimal('source_of_energy_renewable_percentage', 5, 2)->nullable();
+            $table->integer('source_of_energy_generator')->nullable();
+            $table->decimal('source_of_energy_generator_percentage', 5, 2)->nullable();
+            $table->integer('no_source_of_energy')->nullable();
+            $table->decimal('no_source_of_energy_percentage', 5, 2)->nullable();
+            $table->integer('source_of_energy_grand_total')->nullable();
+
+            //national park / protected areas
+            $table->integer('national_parks')->nullable();
+            $table->decimal('national_parks_percentage', 5, 2)->nullable();
+            $table->integer('protected_areas')->nullable();
+            $table->decimal('protected_areas_percentage', 5, 2)->nullable();
+            $table->integer('no_national_parks')->nullable();
+            $table->decimal('no_national_parks_percentage', 5, 2)->nullable();
+            $table->integer('national_parks_grand_total')->nullable();
+
+            //sanitary
+            $table->integer('sanitary_dry_toilets')->nullable();
+            $table->integer('sanitary_wet_toilets')->nullable();
+            $table->integer('sanitary_toilets_separation')->nullable();
+            $table->integer('sanitary_toilets')->nullable();
+            $table->integer('sanitary_showers')->nullable();
+            $table->integer('sanitary_total_with_toilets')->nullable();
+            $table->decimal('sanitary_total_with_toilets_percentage', 5, 2)->nullable();
+            $table->integer('sanitary_total_without_toilets')->nullable();
+            $table->decimal('sanitary_total_without_toilets_percentage', 5, 2)->nullable();
+
+            //kitchen
+            $table->integer('electric_kitchen')->nullable();
+            $table->decimal('electric_kitchen_percentage', 5, 2)->nullable();
+            $table->integer('gas_kitchen')->nullable();
+            $table->decimal('gas_kitchen_percentage', 5, 2)->nullable();
+            $table->integer('wood_kitchen')->nullable();
+            $table->decimal('wood_kitchen_percentage', 5, 2)->nullable();
+            $table->integer('no_kitchen')->nullable();
+            $table->decimal('no_kitchen_percentage', 5, 2)->nullable();
+            $table->integer('kitchen_grand_total')->nullable();
+
+            //types of huts
+            $table->integer('hut_type_hut')->nullable();
+            $table->integer('hut_type_biwak')->nullable();
+            $table->integer('hut_type_cabane')->nullable();
+            $table->integer('hut_type_other')->nullable();
+            $table->integer('hut_type_grand_total')->nullable();
+            $table->integer('hut_type_lowest_elevation')->nullable();
+            $table->integer('hut_type_highest_elevation')->nullable();
+
+            //ownership
+            $table->integer('ownership_mountaineering_association')->nullable();
+            $table->integer('ownership_mountain_club')->nullable();
+            $table->integer('ownership_eos_poa_sox_seo_xos_sho_xoo')->nullable();
+            $table->integer('ownership_municipality')->nullable();
+            $table->integer('ownership_state')->nullable();
+            $table->integer('ownership_private_property')->nullable();
+            $table->integer('ownership_other')->nullable();
+            $table->integer('ownership_grand_total')->nullable();
         });
     }
 
@@ -43,7 +114,80 @@ return new class extends Migration
     public function down()
     {
         Schema::table('hut_surveys', function (Blueprint $table) {
-            //
+            $table->dropColumn([
+                'wastewater_mechanical_plus_biological_treatment',
+                'wastewater_mechanical_plus_biological_treatment_percentage',
+                'wastewater_no_treatment',
+                'wastewater_no_treatment_percentage',
+                'wastewater_public_grid',
+                'wastewater_public_grid_percentage',
+                'wastewater_unspecified',
+                'wastewater_unspecified_percentage',
+                'wastewater_treatment_grand_total',
+                'waste_management_system_yes',
+                'waste_management_system_yes_percentage',
+                'waste_management_system_no',
+                'waste_management_system_no_percentage',
+                'waste_management_system_grand_total',
+                'surface_water_supply',
+                'surface_water_supply_percentage',
+                'ground_well_water_supply',
+                'ground_well_water_supply_percentage',
+                'public_supply',
+                'public_supply_percentage',
+                'no_water_supply',
+                'no_water_supply_percentage',
+                'water_supply_grand_total',
+                'source_of_energy_public_grid',
+                'source_of_energy_public_grid_percentage',
+                'source_of_energy_renewable',
+                'source_of_energy_renewable_percentage',
+                'source_of_energy_generator',
+                'source_of_energy_generator_percentage',
+                'no_source_of_energy',
+                'no_source_of_energy_percentage',
+                'source_of_energy_grand_total',
+                'national_parks',
+                'national_parks_percentage',
+                'protected_areas',
+                'protected_areas_percentage',
+                'no_national_parks',
+                'no_national_parks_percentage',
+                'national_parks_grand_total',
+                'sanitary_dry_toilets',
+                'sanitary_wet_toilets',
+                'sanitary_toilets_separation',
+                'sanitary_toilets',
+                'sanitary_showers',
+                'sanitary_total_with_toilets',
+                'sanitary_total_with_toilets_percentage',
+                'sanitary_total_without_toilets',
+                'sanitary_total_without_toilets_percentage',
+                'electric_kitchen',
+                'electric_kitchen_percentage',
+                'gas_kitchen',
+                'gas_kitchen_percentage',
+                'wood_kitchen',
+                'wood_kitchen_percentage',
+                'no_kitchen',
+                'no_kitchen_percentage',
+                'kitchen_grand_total',
+                'hut_type_hut',
+                'hut_type_biwak',
+                'hut_type_cabane',
+                'hut_type_other',
+                'hut_type_grand_total',
+                'hut_type_lowest_elevation',
+                'hut_type_highest_elevation',
+                'ownership_mountaineering_association',
+                'ownership_mountain_club',
+                'ownership_eos_poa_sox_seo_xos_sho_xoo',
+                'ownership_municipality',
+                'ownership_state',
+                'ownership_private_property',
+                'ownership_other',
+                'ownership_grand_total',
+            ]);
         });
     }
 };
